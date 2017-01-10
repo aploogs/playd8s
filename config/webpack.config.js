2cc2bb0c0c7c958cf6958881d8dd601ec8c02904
Andrew Ploeger <andrewploeger@gmail.com>
2017-01-09 19:48:40 -0700

    remove no errors plugin

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -19,7 +19,6 @@ var config = {
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development')
     })
